7d95f56dabd66204cd7862b9811ddc0f2c3a6283
Ucuuuppp <yrahmatullah111@gmail.com>
2023-04-07 21:17:57 +0700

    Initial commit

---
+++ src/Latihan2.java
@@ -0,0 +1,22 @@
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Latihan2 {
+
+    public void queueExemple() {
+        Queue<String> queue = new LinkedList<String>();
+        queue.add("Java");
+        queue.add("DotNet");
+        queue.offer("PHP");
+        queue.offer("HTML");
+        System.out.println("remove: " + queue.remove());
+        System.out.println("element: " + queue.element());
+        System.out.println("poll: " + queue.poll());
+        System.out.println("peek: " + queue.peek());
+    }
+
+    public static void main(String[] args){
+        new Latihan2() .queueExemple();
+    }
+}
